86f707ce71a59b47872d233d3838f5d0742cb49a
swapnilsharma881 <swapnilsharma881@gmail.com>
2024-02-10 18:53:19 +0530

    Added nodemon deppendency

--- 3.1 Express Server/index.js
+++ 3.1 Express Server/index.js
@@ -2,6 +2,11 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+var x  = 2;
+
+console.log(x);
+
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
