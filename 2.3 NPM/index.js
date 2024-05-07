e9e9cec1ba7dfbeae3cd8adcb83dc52069313e26
Swapnil Vishwakarma <99011495+Swapnilsharma881@users.noreply.github.com>
2024-05-07 11:45:32 +0530

    Delete 2.3 NPM directory

--- 2.3 NPM/index.js
+++
@@ -1,9 +0,0 @@
-import  generateName from "sillyName"
-import superheroes from "superheroes";
-//To enable import statment you need to do this "type": "module" 
-
-var sillyName = generateName();
-
-console.log(`My name is ${sillyName}`);
-
-console.log(`I am ${superheroes.random()}`);
